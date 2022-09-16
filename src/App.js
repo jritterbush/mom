59cf63562fc46db9d340903bcda04024006dc7d7
John Ritterbush <jritterbush@gmail.com>
2022-09-16 12:28:11 -0500

    :rocket:

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useState } from "react";
+import PhotoAlbum from "react-photo-album";
+import photos from "./image-list.json";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+
+const slides = photos.map(({ src, width, height, images }) => ({
+  src,
+  width,
+  height,
+  srcSet:
+    images &&
+    images.map((image) => ({
+      src: image.src,
+      width: image.width,
+      height: image.height,
+    })),
+}));
 
 function App() {
+  const [index, setIndex] = useState(-1);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PhotoAlbum
+        columns={(containerWidth) => {
+          if (containerWidth < 400) return 1;
+        }}
+        layout="masonry"
+        photos={photos}
+        targetRowHeight={150}
+        onClick={(event, photo, index) => setIndex(index)}
+        componentsProps={{ imageProps: { loading: "lazy" } }}
+      />
+      <Lightbox
+        slides={slides}
+        open={index >= 0}
+        index={index}
+        close={() => setIndex(-1)}
+      />
     </div>
   );
 }
